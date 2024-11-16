074fc7825c5662040cc63f12f1e55d91b4216eec
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-11-16 02:50:38 +0100

    Build admin route

---
+++ src/components/AdminEnumerators.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<ul>
+			<v-expansion-panels>
+				<div
+					v-for="(properties, collectionName) in config.enumerators"
+					:key="collectionName"
+					class="wide"
+				>
+					<EnumDropdownItem
+						v-if="shouldBeDisplayed(collectionName)"
+						:collectionName="collectionName as unknown as string"
+						:properties="properties"
+					/>
+				</div>
+			</v-expansion-panels>
+		</ul>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { usePartnersStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import EnumDropdownItem from '@/components/EnumDropdownItem.vue';
+
+const store = usePartnersStore();
+const { config } = storeToRefs(store);
+
+
+function shouldBeDisplayed (name: any) {
+  return name !== "_id";
+}
+</script>
+
+<style scoped>
+.wide {
+	width: 100%;
+}
+
+.left {
+	text-align: left;
+}
+
+.bold {
+	font-weight: bold;
+}
+
+.box {
+	margin: 5px;
+	border: 1px solid #ccc;
+	border-radius: 1px;
+}
+</style>
---
+++ src/components/AdminInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <h1>Admin Dashboard</h1>
+    <v-text-field label="UI Version" v-model="config.uiVersion"></v-text-field>
+    <v-text-field label="API Version" v-model="config.apiVersion"></v-text-field>
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-title><h2>Database Collections</h2></v-expansion-panel-title>
+        <v-expansion-panel-text><AdminCollections/></v-expansion-panel-text>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-title><h2>API Config Items</h2></v-expansion-panel-title>
+        <v-expansion-panel-text><AdminConfigItems/></v-expansion-panel-text>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-title><h2>Enumerators</h2></v-expansion-panel-title>
+        <v-expansion-panel-text><AdminEnumerators/></v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { usePartnersStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import AdminCollections from '@/components/AdminCollections.vue';
+import AdminConfigItems from '@/components/AdminConfigItems.vue';
+import AdminEnumerators from '@/components/AdminEnumerators.vue';
+
+
+const store = usePartnersStore();
+const { config } = storeToRefs(store);
+
+onMounted(() => {
+    document.title = 'Configuration';
+});
+
+</script>
